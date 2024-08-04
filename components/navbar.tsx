3b205be90b7b81fe40f2d3c74457c0e30be0945f
Christopher <contato.christopher@gmail.com>
2024-08-04 19:07:59 -0300

    beautiful navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,7 +49,11 @@ export const Navbar = () => {
   );
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
+    <NextUINavbar 
+      maxWidth="xl" 
+      position="sticky" 
+      className="backdrop-blur-sm bg-black/10" // Aplica o blur e a transparência
+    >
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
